91ea25b8f5afa8bd1202707ccb5c96a9bd28c5ed
5Vladimir5 <nikishin.vov@yandex.ru>
2016-12-06 21:15:59 +0300

    Framework
    framework

---
+++ lib/Dbconnect.php
@@ -0,0 +1,47 @@
+<?php
+class Dbconnect
+{
+    private static $_instance;
+    private $_idConnect;
+	
+    private $_configs = array();
+	
+    private function __construct(){}
+    private function __clone(){}
+	
+    public static function instance ()
+    {
+        if(empty(self::$_instance)) 
+			self::$_instance = new self();
+        return self::$_instance;
+	}
+    public function set($array)
+    {
+        $this->_configs = array
+        (
+           'host' => $array['host'],
+           'user' => $array['user'],
+           'pass' => $array['pass'],
+           'name' => $array['name']            
+        );
+    }
+    
+    public function connect ()
+    {
+        self::$_idConnect = DbSimple_Generic::connect("mysql://".
+        $this->_configs['user'].":".
+        $this->_configs['pass']."@".
+        $this->_configs['host']."/".
+        $this->_configs['name']);
+		
+       $this->idConnect->setIdentPrefix(TABLE_PREFIX);
+		
+        if(Config::instance()->get('dev_mode')==0) {
+			$this->_idConnect->setErrorHandler('databaseErrorHandler');
+    }
+	}
+    public function getConnect()
+    {
+        return $this->_idConnect;
+    }
+}
